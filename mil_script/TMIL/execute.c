86e316ee2b06fdc7655733fc8f14bf123c3859c4
aburafia <abukame@gmail.com>
2010-08-04 18:19:02 +0900

    デバッグプリント

--- mil_script/TMIL/execute.c
+++ mil_script/TMIL/execute.c
@@ -29,143 +29,191 @@ void mvm_execute(void){
   while (pc < g_bytecode_size) {
     switch (g_bytecode[pc]) {
     case OP_PUSH_INT:
-      lg("OP_PUSH_INT");
+      fprintf(stderr, "OP_PUSH_INT pc[%d] sp[%d]\n",pc, sp);
 
       st_stack[sp].type = INT_VALUE_TYPE;
       st_stack[sp].u.int_value
         = (int)g_bytecode[pc+1];
       sp++;
       pc += 2;
+      
+      exeprint();
+      
       break;
     case OP_PUSH_STRING:
-      lg("OP_PUSH_STRING");
+      fprintf(stderr, "OP_PUSH_STRING pc[%d] sp[%d]\n",pc, sp);
 
       st_stack[sp].type = STRING_VALUE_TYPE;
       st_stack[sp].u.string_value
         = g_str_pool[g_bytecode[pc+1]];
       sp++;
       pc += 2;
+
+      exeprint();
+
       break;
     case OP_ADD:
-      lg("OP_ADD");
+      fprintf(stderr, "OP_ADD pc[%d] sp[%d]\n",pc, sp);
 
       st_stack[sp-2].u.int_value
         = st_stack[sp-2].u.int_value
         + st_stack[sp-1].u.int_value;
       sp--;
       pc++;
+      
+      exeprint();
+
       break;
     case OP_SUB:
-      lg("OP_SUB");
+      fprintf(stderr, "OP_SUB pc[%d] sp[%d]\n",pc, sp);
 
       st_stack[sp-2].u.int_value
         = st_stack[sp-2].u.int_value
         - st_stack[sp-1].u.int_value;
       sp--;
       pc++;
+
+      exeprint();
+
       break;
     case OP_MUL:
-      lg("OP_MUL");
+      fprintf(stderr, "OP_MUL pc[%d] sp[%d]\n",pc, sp);
 
       st_stack[sp-2].u.int_value
         = st_stack[sp-2].u.int_value
         * st_stack[sp-1].u.int_value;
       sp--;
       pc++;
+
+      exeprint();
+
       break;
     case OP_DIV:
-      lg("OP_DIV");
+      fprintf(stderr, "OP_DIV pc[%d] sp[%d]\n",pc, sp);
 
       st_stack[sp-2].u.int_value
         = st_stack[sp-2].u.int_value
         / st_stack[sp-1].u.int_value;
       sp--;
       pc++;
+
+      exeprint();
+
       break;
     case OP_MINUS:
-      lg("OP_MINUS");
+      fprintf(stderr, "OP_MINUS pc[%d] sp[%d]\n",pc, sp);
 
       st_stack[sp-1].u.int_value
         = -st_stack[sp-1].u.int_value;
       pc++;
+
+      exeprint();
+
       break;
     case OP_EQ:
-      lg("OP_EQ");
+      fprintf(stderr, "OP_EQ pc[%d] sp[%d]\n",pc, sp);
 
       st_stack[sp-2].u.int_value
         = st_stack[sp-2].u.int_value
         == st_stack[sp-1].u.int_value;
       sp--;
       pc++;
+
+      exeprint();
+
       break;
     case OP_NE:
-      lg("OP_NE");
+      fprintf(stderr, "OP_NE pc[%d] sp[%d]\n",pc, sp);
 
       st_stack[sp-2].u.int_value
         = st_stack[sp-2].u.int_value
         != st_stack[sp-1].u.int_value;
       sp--;
       pc++;
+
+      exeprint();
+
       break;
     case OP_GT:
-      lg("OP_GT");
+      fprintf(stderr, "OP_GT pc[%d] sp[%d]\n",pc, sp);
 
       st_stack[sp-2].u.int_value
         = st_stack[sp-2].u.int_value
         > st_stack[sp-1].u.int_value;
       sp--;
       pc++;
+
+      exeprint();
+
       break;
     case OP_GE:
-      lg("OP_GE");
+      fprintf(stderr, "OP_GE pc[%d] sp[%d]\n",pc, sp);
 
       st_stack[sp-2].u.int_value
         = st_stack[sp-2].u.int_value
         >= st_stack[sp-1].u.int_value;
       sp--;
       pc++;
+
+      exeprint();
+
       break;
     case OP_LT:
-      lg("OP_LT");
+      fprintf(stderr, "OP_LT pc[%d] sp[%d]\n",pc, sp);
 
       st_stack[sp-2].u.int_value
         = st_stack[sp-2].u.int_value
         < st_stack[sp-1].u.int_value;
       sp--;
       pc++;
+
+      exeprint();
+
       break;
     case OP_LE:
-      lg("OP_LE");
+      fprintf(stderr, "OP_LE pc[%d] sp[%d]\n",pc, sp);
 
       st_stack[sp-2].u.int_value
         = st_stack[sp-2].u.int_value
         <= st_stack[sp-1].u.int_value;
       sp--;
       pc++;
+
+      exeprint();
+
       break;
     case OP_PUSH_VAR:
-      lg("OP_PUSH_VAR");
+      fprintf(stderr, "OP_PUSH_VAR pc[%d] sp[%d]\n",pc, sp);
 
       st_stack[sp]
         = st_variable[(int)g_bytecode[pc+1]];
       sp++;
       pc += 2;
+
+      exeprint();
+
       break;
     case OP_ASSIGN_TO_VAR:
-      lg("OP_ASSIGN_TO_VAR");
+      fprintf(stderr, "OP_ASSIGN_TO_VAR pc[%d] sp[%d]\n",pc, sp);
 
       st_variable[(int)g_bytecode[pc+1]]
         = st_stack[sp-1];
       sp--;
       pc += 2;
+
+      exeprint();
+
       break;
     case OP_JUMP:
-      lg("OP_JUMP");
+      fprintf(stderr, "OP_JUMP pc[%d] sp[%d]\n",pc, sp);
 
       pc = (int)g_bytecode[pc+1];
+
+      exeprint();
+
       break;
     case OP_JUMP_IF_ZERO:
-      lg("OP_JUMP_IF_ZERO");
+      fprintf(stderr, "OP_JUMP_IF_ZERO pc[%d] sp[%d]\n",pc, sp);
 
       if (!st_stack[sp-1].u.int_value) {
         pc = (int)g_bytecode[pc+1];
@@ -173,22 +221,31 @@ void mvm_execute(void){
         pc += 2;
       }
       sp--;
+
+      exeprint();
+
       break;
     case OP_GOSUB:
-      lg("OP_GOSUB");
+      fprintf(stderr, "OP_GOSUB pc[%d] sp[%d]\n",pc, sp);
 
       st_stack[sp].u.int_value = pc + 2;
       sp++;
       pc = (int)g_bytecode[pc+1];
+
+      exeprint();
+
       break;
     case OP_RETURN:
-      lg("OP_RETURN");
+      fprintf(stderr, "OP_RETURN pc[%d] sp[%d]\n",pc, sp);
 
       pc = st_stack[sp-1].u.int_value;
       sp--;
+
+      exeprint();
+
       break;
     case OP_PRINT:
-      lg("OP_PRINT");
+      fprintf(stderr, "OP_PRINT pc[%d] sp[%d]\n",pc, sp);
 
       if(st_stack[sp-1].type == INT_VALUE_TYPE){
         printf("%d\n", st_stack[sp-1].u.int_value);
@@ -197,6 +254,9 @@ void mvm_execute(void){
       }
       sp--;
       pc++;
+
+      exeprint();
+
       break;
     default:
       assert(0);
